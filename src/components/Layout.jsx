9c3749604df6daac09165071c2c2c40d95263b9a
johnwinifred <MY_NAME@example.com>
2024-07-11 03:32:44 +0100

    made changes to show navbar poduct count

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
     return (
       <>
         <section className='flex flex-col justify-between px-8 py-4 bg-[#E6F2E6] '>
-          <Navbar />
+          <Navbar productsInCart={productsInCart} setCartVisible={setCartVisible} />
           <div className="">
           <div className="">{children}</div>
           </div>
